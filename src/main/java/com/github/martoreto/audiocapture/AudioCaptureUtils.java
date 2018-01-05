f4ceb0ddbc5f5d7d024fe139c952c5d1a636a710
martoreto <martoreto1@gmail.com>
2018-01-06 00:42:18 +0100

    support self-systemization when rooted

---
+++ src/main/java/com/github/martoreto/audiocapture/AudioCaptureUtils.java
@@ -0,0 +1,52 @@
+package com.github.martoreto.audiocapture;
+
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+
+public class AudioCaptureUtils {
+    public static final String PERMISSION_CAPTURE_AUDIO = "com.github.martoreto.audiocapture.REDIRECT_AUDIO";
+    public static final String PERMISSION_MONITOR_MEDIA = "com.github.martoreto.audiocapture.MONITOR_PLAYING_MEDIA";
+
+    public static final String PACKAGE_NAME = "com.github.martoreto.audiocapture";
+    public static final String ACTION_CAPTURE_AUDIO = PACKAGE_NAME + ".SERVICE";
+
+    private static final String PERMISSION_TO_CHECK = "android.permission.MODIFY_AUDIO_ROUTING";
+
+    public static boolean isAudioCapturePrivSystemApp(PackageManager packageManager) {
+        try {
+            PackageInfo info = packageManager.getPackageInfo(PACKAGE_NAME, PackageManager.GET_PERMISSIONS);
+            for (int i = 0; i < info.requestedPermissions.length; i++) {
+                if (PERMISSION_TO_CHECK.equals(info.requestedPermissions[i])) {
+                    return (info.requestedPermissionsFlags[i] & PackageInfo.REQUESTED_PERMISSION_GRANTED) != 0;
+                }
+            }
+            return false;
+        } catch (PackageManager.NameNotFoundException e) {
+            return false;
+        }
+    }
+
+    public static boolean isAudioCaptureSystemApp(PackageManager packageManager) {
+        try {
+            PackageInfo info = packageManager.getPackageInfo(PACKAGE_NAME, 0);
+            return info.applicationInfo != null &&
+                    (info.applicationInfo.flags & ApplicationInfo.FLAG_SYSTEM) != 0;
+        } catch (PackageManager.NameNotFoundException e) {
+            return false;
+        }
+    }
+
+    public static boolean isAudioCaptureInstalled(PackageManager packageManager) {
+        return isPackageInstalled(PACKAGE_NAME, packageManager);
+    }
+
+    private static boolean isPackageInstalled(String packageName, PackageManager packageManager) {
+        try {
+            packageManager.getPackageInfo(packageName, 0);
+            return true;
+        } catch (PackageManager.NameNotFoundException e) {
+            return false;
+        }
+    }
+}
