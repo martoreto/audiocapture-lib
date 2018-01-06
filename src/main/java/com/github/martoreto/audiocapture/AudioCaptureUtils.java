64fbc029416c50d74ab7bcb76f60b30d334811e8
martoreto <martoreto1@gmail.com>
2018-01-06 14:47:46 +0100

    better systemization UX

--- src/main/java/com/github/martoreto/audiocapture/AudioCaptureUtils.java
+++ src/main/java/com/github/martoreto/audiocapture/AudioCaptureUtils.java
@@ -1,5 +1,8 @@
 package com.github.martoreto.audiocapture;
 
+import android.app.Activity;
+import android.content.ComponentName;
+import android.content.Intent;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
@@ -10,6 +13,7 @@ public class AudioCaptureUtils {
 
     public static final String PACKAGE_NAME = "com.github.martoreto.audiocapture";
     public static final String ACTION_CAPTURE_AUDIO = PACKAGE_NAME + ".SERVICE";
+    public static final String ACTION_SETUP = PACKAGE_NAME + ".SETUP";
 
     private static final String PERMISSION_TO_CHECK = "android.permission.MODIFY_AUDIO_ROUTING";
 
@@ -48,5 +52,30 @@ public class AudioCaptureUtils {
         } catch (PackageManager.NameNotFoundException e) {
             return false;
         }
+    }
+
+    public static boolean requestSetup(int requestCode, Activity activity) {
+        PackageManager pm = activity.getPackageManager();
+        boolean needsInstall = false;
+        try {
+            PackageInfo info = pm.getPackageInfo(PACKAGE_NAME, PackageManager.GET_PERMISSIONS);
+            for (int i = 0; i < info.requestedPermissions.length; i++) {
+                if ((info.requestedPermissionsFlags[i] & PackageInfo.REQUESTED_PERMISSION_GRANTED) == 0) {
+                    needsInstall = true;
+                }
+            }
+            if (needsInstall) {
+                Intent intent = new Intent(ACTION_SETUP);
+                intent.setPackage(PACKAGE_NAME);
+                activity.startActivityForResult(intent, requestCode);
+                return true;
+            } else {
+                return false;
+            }
+        } catch (PackageManager.NameNotFoundException e) {
+            // Not even installed, nothing to do here...
+            return true;
+        }
+
     }
 }
